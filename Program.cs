c39b65210c7d543c5bbab13dd758dff6a6542bb3
polarecreater <hmst193@gmail.com>
2022-01-05 21:27:16 +0900

    initial commit, enter name
    220105

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*-----------------------------------------------------------------------------
+ * Name: _002_HelloWorld2
+ * DESC: 메인 함수의 파라미터 보기
+-----------------------------------------------------------------------------*/
+namespace csharp_grammar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            Console.WriteLine("Enter your first name: ");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            lname = Console.ReadLine();
+            Console.WriteLine("Your full name is " + fname + lname);
+        }
+    }
+}
