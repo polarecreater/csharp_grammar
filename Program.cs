02cf705cb3c1d4ae37fd635d5770587016b4b4c7
polarecreater <hmst193@gmail.com>
2022-01-06 10:17:34 +0900

    5-2 배열 크기 최댓값, 최솟값
    220106 잘 모르겠음

--- Program.cs
+++ Program.cs
@@ -5,12 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*-----------------------------------------------------------------------------
- * Name: Q_Snail
- * DESC: 자료구조 오티 달팽이 문제
- * 땅 위에 달팽이가 있다. 이 달팽이는 높이가 10미터인 나무를  올라가는 중이다. 
- * 달팽이는 낮에 3미터를 올라가고, 밤 잠을 자는 동안 2미터를 미끄러진다.
- * 달팽이가 나무를 모두 올라가려면 며칠이 걸리는지 구하시오.
- * 데이터 : int 낮 = 3; int 밤 = -2; int 나무 = 10;
+ * Name: 배열 개념 코드
+ * DESC: 자료구조 배열
+ * 
 -----------------------------------------------------------------------------*/
 namespace csharp_grammar
 {
@@ -18,35 +15,15 @@ namespace csharp_grammar
     {
         static void Main(string[] args)
         {
-            int day = 3;//올라가는 높이
-            int night = 2;//내려가는 높이
-            int tree = 10;//나무 높이
-            int goup = 0;//현재 높이
-            int daynum = 0;//걸린 날짜
-            /*
-            //내가 쓴 코드 
-            while(goup <= tree){
-                daynum++;
-                goup += day;
-                //여기에서 비교를 해야 함
-                goup -= night;
-                Console.Write(daynum);
+            //int 최댓값
+            int n = int.MaxValue;
+            //int 최솟값
+            int a = int.MinValue;
+            n = 0;
+            if(n!=int.MaxValue){
+                //처리 코드
             }
-            Console.WriteLine();
-            Console.WriteLine(daynum);
-            */
-            
-            while(true){
-                daynum++;
-                goup += day;
-                if(goup >= tree){
-                    break;
-                }
-                goup -= night;
-                Console.Write(daynum);
-            }
-            Console.WriteLine();
-            Console.WriteLine(daynum);
+
             
         }
     }
