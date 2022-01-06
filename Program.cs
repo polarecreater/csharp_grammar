d02872361eb41d818dc1d1e1ca65ff593e1b626b
polarecreater <hmst193@gmail.com>
2022-01-06 13:10:55 +0900

    5-2-2 List<T> 제네릭 리스트
    220106

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*-----------------------------------------------------------------------------
- * Name: ArrayList
- * DESC: 자료구조 ArrayList
+ * Name: List<T> 
+ * DESC: 자료구조 List<T>
  * 예시 코드
 -----------------------------------------------------------------------------*/
 namespace csharp_grammar
@@ -17,9 +17,21 @@ namespace csharp_grammar
         //강의 코드
         static void Main(string[] args)
         {
-            int n = 100;
-            object o =n;
-            int b = (int)o;//명시적 변경 필요
+            List<string> list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            //list.Add(22);//데이터 타입이 맞지 않아 오류
+
+            list.Insert(1,"c");//삽입
+            list.Remove("a");//값 삭제
+            list.RemoveAt(1);//순서 삭제
+
+            for (int i = 0; i < list.Count; i++)
+            {   
+                Console.WriteLine(list[i]);//출력
+                
+            }
+
         }
         
     }
