170b4ecc9765b3adcbdc44f41cbb5e4ae7ff532c
polarecreater <hmst193@gmail.com>
2022-01-06 13:05:47 +0900

    5-2-2 ArrayList 형변환
    220106

--- Program.cs
+++ Program.cs
@@ -17,18 +17,9 @@ namespace csharp_grammar
         //강의 코드
         static void Main(string[] args)
         {
-            ArrayList list = new ArrayList();
-            list.Add("문자열");//0번 데이터
-            list.Add(100);//1번 데이터
-            list.Add(true);//2번 데이터
-            
-            list.Insert(2,200);//2번 위치에 데이터 추가, 원래 2번->3번
-            list.RemoveAt(1);//1변 데이터 삭제
-            list.Remove("문자열"); //"문자열"에 해당하는 값 삭제
-
-            for(int i = 0; i<list.Count; i++){//리스트 갯수만큼 반복
-                Console.WriteLine(list[i]);
-            }
+            int n = 100;
+            object o =n;
+            int b = (int)o;//명시적 변경 필요
         }
         
     }
