fc1c20bdb08bbfc0289a6314b01dea8de92e8f5f
polarecreater <hmst193@gmail.com>
2022-01-07 14:23:23 +0900

    5-2-8 hashtable, dictionary<T>
    220107

--- Program.cs
+++ Program.cs
@@ -6,73 +6,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*-----------------------------------------------------------------------------
- * Name: Queue
- * DESC: 자료구조 원형 Queue를 배열로 구현
- * 데이터: int front, rear, index; 배열;
+ * Name: Hashtable, Dictionary<T>
+ * DESC: 자료구조 Hashtable, Dictionary<T>
+ * 
 -----------------------------------------------------------------------------*/
 namespace csharp_grammar
 {
     class Program
     {
         //강의 코드
-        static int[] m_queue = new int[5];//왜 static?
-        static int front = -1;
-        static int rear = -1;
-        
         static void Main(string[] args)
         {
-            Enqueue(10);
-            Enqueue(20);
-            Enqueue(30);
-            Enqueue(40);
-            Enqueue(50);
+            //해쉬 테이블
+            Hashtable hash = new Hashtable();
+            hash.Add(0, "하지");
+            hash.Add(1, "동지");
+            hash.Add(2, "소한");
+            hash.Add(3, "대한");
+            Console.WriteLine(hash[3]);//key를 인덱스처럼 사용 가능
 
-            Dequeue();
+            //딕셔너리
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("하지","001");
+            dic.Add("동지","002");
+            dic.Add("소한","003");
+            dic.Add("대한","004");
 
-            Enqueue(60);//데이터 꽉 참
+            // //딕셔너리는 중복이 안됨
+            // dic.Add("하지","006");//넣어지지 않음
 
-            Enqueue(70);//들어가지 않음
+            //키가 이미 있는지 확인
+            if(!dic.ContainsKey("경칩")){
+                dic.Add("경칩","005");
+            }
 
-            Dequeue();
-            Dequeue();
-            Dequeue();
-
-            Enqueue(80);
-            Enqueue(90);
+            //삭제
+            dic.Remove("소한");
             
-            Dequeue();
-            Dequeue();
-            Dequeue();
-            for (int i = 0; i < m_queue.Length; i++)
+            foreach (var item in dic.Keys)//키 개수만큼 반복
             {
-                Console.WriteLine("Index" + i + " - " + m_queue[i]);
-            }
-        }
-        private static void Enqueue(int value){
-            rear++;
-
-            //데이터 꽉 찾을 때 대비
-            if(rear - front>m_queue.Length -1){//배열의 값 넘어간 경우
-                rear--;//원상 복귀
-                return;//데이터 넣지 않은 상태로 리턴
-            }
-            m_queue[rear%m_queue.Length] = value;//배열 인덱스 꽉 찼을 때 대비
-
-            if(front == -1)//처음 수가 들어올 때
-                front = 0;
-        }
-        private static void Dequeue(){
-            if(front == -1){//아무것도 없을 때
-                return;
-            }
-            int value = m_queue[front%m_queue.Length];//dequeue를 무한정으로 할 경우
-            Console.WriteLine("Dequeue - "+value);
-            m_queue[front % m_queue.Length] = 0;
-            front++;
-
-            if(front>rear){//데이터 다 뽑아낸 경우
-                front = -1;
-                rear = -1;
+                Console.WriteLine(item + ": " + dic[item]);
             }
         }
     }
