968afb6194ae5245cd86236a5060a5bbef0e2717
polarecreater <hmst193@gmail.com>
2022-01-08 21:15:00 +0900

    5-3-4 퀵정렬
    220108 이해가 안되서 4시간 정도 써가면서 고군분투 했다. 하나하나씩 출력을 하니까 이해했다.

--- Program.cs
+++ Program.cs
@@ -5,27 +5,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*-----------------------------------------------------------------------------
- * Name: 재귀 호출 피보나치
- * DESC: 재귀 호출 피보나치 강의 코드
+ * Name: 퀵 정렬
+ * DESC: 퀵 정렬 강의 코드
  * 
 -----------------------------------------------------------------------------*/
 namespace csharp_grammar
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int nValue = 10;
-            Console.WriteLine("Fibonacci " + nValue + " - " + FuncFibonacci(nValue));
-        }
-        static int FuncFibonacci(int nSequence){
-            int nResult = 0;
-            if(nSequence == 1 || nSequence == 2){
-                nResult = 1;
-            }else{
-                nResult = FuncFibonacci(nSequence - 1) + FuncFibonacci(nSequence - 2);
+        static int[] data = { 25, 15, 60, 45, 10, 20, 5, 30};
+
+        static void Main(string[] args){
+            Console.Write("시작값 : ");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i].ToString() + ", ");
             }
-            return nResult;
+            Console.WriteLine();
+            SortQuick(0, data.Length -1);
+            Console.Write("정렬값 : ");
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.Write(data[i].ToString() + ",");
+            }
+            Console.WriteLine();
+        }
+        static void SortQuick(int nFirst, int nLast){
+            
+            if (nFirst<nLast){
+                int pivotIndex = FuncPartition(nFirst, nLast);
+                
+                //분할
+                SortQuick(nFirst, pivotIndex - 1);
+                SortQuick(pivotIndex + 1, nLast);
+            }
+        }
+        static int FuncPartition(int nFirst, int nLast){
+            int nLow, nHigh, nPivot;
+
+            //임의의 값 - 여기서는 마지막 값
+            nPivot = data[nLast];
+
+            nLow = nFirst;
+            nHigh = nLast - 1;
+
+            Console.WriteLine("Low : " + data[nLow] + ", High : " + data[nHigh]);
+            
+            while(nLow <= nHigh){
+                while (data[nLow] < nPivot){
+                    nLow++;
+                    Console.WriteLine("Low Change : "+data[nLow]);
+                }
+                while (data[nHigh] > nPivot){
+                    nHigh--;
+                    Console.WriteLine("High Change : "+data[nHigh]);
+                }
+                if(nLow <= nHigh){
+                    Swap(data, nLow, nHigh);
+                }
+                Console.WriteLine("Low : " + data[nLow] + ", High : " + data[nHigh]);
+
+            }
+            Swap(data, nLow, nLast);//잘 모르겠다..?
+
+            Console.Write("정렬값 (Pivot : " + nPivot + ") : ");
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                if(nPivot == data[i])
+                    Console.Write("*" + data[i] + "*, ");
+                else
+                    Console.Write(data[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            return nLow;
+        }
+        static void Swap(int[] nArrData, int nValue1, int nValue2){
+            int nTemp = nArrData[nValue1];
+            nArrData[nValue1] = nArrData[nValue2];
+            nArrData[nValue2] = nTemp;
+
+            Console.WriteLine("Swap : " + nArrData[nValue1]+" , "+ nArrData[nValue2]);
         }
     }
 }
